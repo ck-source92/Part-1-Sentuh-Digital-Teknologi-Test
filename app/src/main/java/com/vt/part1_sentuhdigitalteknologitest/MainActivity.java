38ad00b8560488bb9367865026ba2eb9e3e31cd2
Dwi Candra Kirana <dwicandra920@gmail.com>
2023-11-23 13:01:07 +0700

    Initial commit

---
+++ app/src/main/java/com/vt/part1_sentuhdigitalteknologitest/MainActivity.java
@@ -0,0 +1,14 @@
+package com.vt.part1_sentuhdigitalteknologitest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
